ecd21739a1ae31da73688cab5029c50f72275523
Dilnaz <dilnaz@whatever.com>
2024-04-18 16:16:04 +0600

    .

--- lab6/buil.py
+++ lab6/buil.py
@@ -48,15 +48,3 @@ millisec = int(input())
 
 calc(num, millisec)
 
-#ex5
-def all_true(tup):
-    return all(tup)
-
-my_tup = (True, True, True, True)
-print("All elements of the my_tup are true:", all_true(my_tup))
-
-my_tup = (True, False)
-print("All elements of the my_tup are true:", all_true(my_tup))
-
-my_tup = (False, False, False)
-print("All elements of the my_tup are true:", all_true(my_tup))
